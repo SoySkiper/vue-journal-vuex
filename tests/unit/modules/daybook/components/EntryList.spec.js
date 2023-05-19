f9e023ed3c1ad1dc557b2d19394419af7fc0db6c
SoySkiper <soyskiper@gmail.com>
2023-05-18 18:25:35 -0600

    test EntryList calls, trigger in mockRouter

--- tests/unit/modules/daybook/components/EntryList.spec.js
+++ tests/unit/modules/daybook/components/EntryList.spec.js
@@ -1,41 +1,68 @@
 import { createStore } from 'vuex'
-import { getEntriesByTerm } from '@/modules/daybook/store/journal/getters';
+import journal from "@/modules/daybook/store/journal"
 import { journalState } from '../../../mock-data/test-journal-state';
 import { shallowMount } from '@vue/test-utils';
 import EntryListVue from '@/modules/daybook/components/EntryList.vue';
 
-describe('Pruebas en EntryList', () => {
-    
-    const journalMockModule = {
-        namespaced: true,
-        getters: {
-            getEntriesByTerm
-        },
-        state: {
-            isLoading: false,
-            entries: journalState.entries
+// const journalMockModule = {
+//     namespaced: true,
+//     getters: {
+//         getEntriesByTerm
+//     },
+//     state: {
+//         isLoading: false,
+//         entries: journalState.entries
+//     }
+// }
+// const store = createStore({
+//     modules: {
+//         journal: { ...journalMockModule }
+//     }
+// })
+
+const createVuexStore = ( initialState ) => createStore({
+    modules: {
+        journal: {
+            ...journal,
+            state: { ...initialState }
         }
     }
+})
 
-    const store = createStore({
-        modules: {
-            journal: { ...journalMockModule }
-        }
+describe('Pruebas en EntryList', () => {
+    
+    const store = createVuexStore( journalState )
+    const mockRouter = {
+        push: jest.fn()
+    }
+
+    let wrapper
+
+    beforeEach(() => {
+        jest.clearAllMocks()
+        wrapper = shallowMount( EntryListVue, {
+            global: {
+                mocks: {
+                    $router: mockRouter
+                },
+                plugins: [ store ]
+            }
+        })
     })
-
-    const wrapper = shallowMount( EntryListVue, {
-        global: {
-            mocks: {
-                // $router:
-            },
-            plugins: [ store ]
-        }
-    })
-
     
 
     test('debe de llamar el getEntriesByTerm sin termino y mostrar 2 entradas.', () => {
-        console.log(wrapper.html())
-        
+        expect( wrapper.findAll('entry-stub').length ).toBe( 2 )
+        expect( wrapper.html() ).toMatchSnapshot()
+    });
+
+    test('debe de llamar el getEntriesByTerm y filtrar las entradas', async() => {
+        const input = wrapper.find('input')
+        await input.setValue('pomme')
+        expect( wrapper.findAll('entry-stub').length ).toBe( 1 )
+    })
+    test('el boton de nuevo debe de redireccionar a /new', () => {
+        wrapper.find('button').trigger('click')
+        expect( mockRouter.push ).toHaveBeenCalledWith({ name: 'entry', params: { id: 'new'}})
     });
 })
